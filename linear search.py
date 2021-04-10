ae618740076bff264c84ef1b58a1330a9456c761
nationalmenaria <65455071+nationalmenaria@users.noreply.github.com>
2021-04-10 13:53:03 +0530

    Add files via upload

---
+++ linear search.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def linearSearch(array, n, x):
+    for  i in range(0, n):
+        if (array[i] == x):
+            return i
+    return -1
+    
+array = [2, 4, 0, 1, 9]
+x = 1
+n = len(array)
+result = linearSearch(array, n, x)
+
+if(result == -1):
+    print("element not found")
+else:
+    print("element found at index:", result)
